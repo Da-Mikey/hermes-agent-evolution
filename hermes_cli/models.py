Carrie111998/Hermes

feat(providers): add OpenCode Free provider with keyed auth and opencode User-Agent
Adds an OpenCode Free provider plugin. Free model discovery uses models.dev
(cost.input == 0 AND status != "deprecated"), matching opencode CLI's exact
filter logic.

The free tier requires a real account API key and throttles third-party
clients by User-Agent:

- With OPENCODE_FREE_API_KEY configured, the key is sent as a Bearer token
  and requests identify as "opencode/latest".
- Without a key, the keyless fallback strips the SDK's always-injected empty
  Authorization header and still sends the opencode User-Agent.
- The credential resolver no longer blanks OPENCODE_FREE_API_KEY
  unconditionally (the stale keyless-tier assumption), and credential-pool
  exhaustion no longer surfaces the misleading "Set OPENCODE_FREE_API_KEY"
  message.

Co-authored-by: Jean-François <jfm@laposte.net>
Signed-off-by: Rudraksh Chahal <131520192+rudrakshchahal@users.noreply.github.com>

hermes_cli/models.py
@@ -497,6 +497,12 @@ _PROVIDER_MODELS: dict[str, list[str]] = {
         "north-mini-code-free",
         "nemotron-3-ultra-free",
     ],
+    "opencode-free": [
+        "big-pickle",
+        "deepseek-v4-flash-free",
+        "mimo-v2.5-free",
+        "nemotron-3-super-free",
+    ],
     "opencode-go": [
         "kimi-k3",
         "kimi-k2.7-code",
@@ -1209,7 +1215,7 @@ PROVIDER_GROUPS: dict[str, tuple[str, str, list[str]]] = {
     "google":   ("Google Gemini",   "Google AI Studio (API key)",                     ["gemini"]),
     "openai":   ("OpenAI",          "ChatGPT/Codex subscription or direct OpenAI API", ["openai-codex", "openai-api"]),
     "qwen":     ("Qwen",            "Qwen Cloud / DashScope, Coding Plan & Qwen CLI OAuth", ["alibaba", "alibaba-coding-plan", "qwen-oauth"]),
-    "opencode": ("OpenCode",        "Zen pay-as-you-go or Go subscription",            ["opencode-zen", "opencode-go"]),
+    "opencode": ("OpenCode",        "Zen pay-as-you-go, Go subscription, or free tier", ["opencode-zen", "opencode-go", "opencode-free"]),
     "copilot":  ("GitHub Copilot",  "GitHub token API or copilot --acp process",       ["copilot", "copilot-acp"]),
 }
 
@@ -3267,6 +3273,32 @@ def provider_model_ids(provider: Optional[str], *, force_refresh: bool = False) 
                 return ids
         except Exception:
             pass
+
+    # OpenCode Free: match the exact same logic as opencode CLI.
+    # models.dev has cost+status metadata missing from /zen/v1/models.
+    # Filter: cost.input == 0 (free) AND status != "deprecated".
+    if normalized == "opencode-free":
+        try:
+            from agent.models_dev import fetch_models_dev
+            data = fetch_models_dev()
+            provider_data = data.get("opencode")
+            if isinstance(provider_data, dict):
+                mdev_models = provider_data.get("models", {})
+                if isinstance(mdev_models, dict):
+                    free_active = [
+                        mid for mid, m in mdev_models.items()
+                        if isinstance(m, dict)
+                        and isinstance(m.get("cost"), dict)
+                        and m["cost"].get("input") == 0
+                        and m.get("status") != "deprecated"
+                    ]
+                    if free_active:
+                        return sorted(free_active)
+        except Exception:
+            pass
+        curated_static = list(_PROVIDER_MODELS.get(normalized, []))
+        if curated_static:
+            return curated_static
 
     # ── Profile-based generic live fetch (all simple api-key providers) ──
     # Handles any provider registered in providers/ with auth_type="api_key".

Da-Mikey/hermes-agent-evolution

hermes_cli/models.py
@@ -534,6 +534,21 @@ _PROVIDER_MODELS: dict[str, list[str]] = {
         "north-mini-code-free",
         "nemotron-3-ultra-free",
     ],
+    # OpenCode free tier — keyless (no OpenCode account needed). Synced
+    # against live GET /zen/v1/models + anonymous probes (2026-08-21);
+    # deepseek-v4-flash-free delisted (promo ended, now 401s).
+    "opencode-free": [
+        "x-preview-f-free",  # "Ox Alpha" stealth model — free, 1M ctx, ZDR
+        "big-pickle",
+        "hy3-free",
+        "laguna-s-2.1-free",
+        "mimo-v2.5-free",
+        "nemotron-3-ultra-free",
+        "nemotron-3.5-lightning-free",
+        "muse-spark-1.2-contributor-free",
+    ],
+    # Synced against https://opencode.ai/docs/go/ + live GET /zen/go/v1/models
+    # (2026-08-20).
     "opencode-go": [
         "kimi-k3",
         "kimi-k2.7-code",
@@ -1246,7 +1261,7 @@ PROVIDER_GROUPS: dict[str, tuple[str, str, list[str]]] = {
     "google":   ("Google Gemini",   "Google AI Studio (API key)",                     ["gemini"]),
     "openai":   ("OpenAI",          "ChatGPT/Codex subscription or direct OpenAI API", ["openai-codex", "openai-api"]),
     "qwen":     ("Qwen",            "Qwen Cloud / DashScope, Coding Plan & Qwen CLI OAuth", ["alibaba", "alibaba-coding-plan", "qwen-oauth"]),
-    "opencode": ("OpenCode",        "Zen pay-as-you-go or Go subscription",            ["opencode-zen", "opencode-go"]),
+    "opencode": ("OpenCode",        "Zen pay-as-you-go, Go subscription, or free tier", ["opencode-zen", "opencode-go", "opencode-free"]),
     "copilot":  ("GitHub Copilot",  "GitHub token API or copilot --acp process",       ["copilot", "copilot-acp"]),
 }
 
@@ -3311,6 +3326,32 @@ def provider_model_ids(provider: Optional[str], *, force_refresh: bool = False) 
                 return ids
         except Exception:
             pass
+
+    # OpenCode Free: match the exact same logic as opencode CLI.
+    # models.dev has cost+status metadata missing from /zen/v1/models.
+    # Filter: cost.input == 0 (free) AND status != "deprecated".
+    if normalized == "opencode-free":
+        try:
+            from agent.models_dev import fetch_models_dev
+            data = fetch_models_dev()
+            provider_data = data.get("opencode")
+            if isinstance(provider_data, dict):
+                mdev_models = provider_data.get("models", {})
+                if isinstance(mdev_models, dict):
+                    free_active = [
+                        mid for mid, m in mdev_models.items()
+                        if isinstance(m, dict)
+                        and isinstance(m.get("cost"), dict)
+                        and m["cost"].get("input") == 0
+                        and m.get("status") != "deprecated"
+                    ]
+                    if free_active:
+                        return sorted(free_active)
+        except Exception:
+            pass
+        curated_static = list(_PROVIDER_MODELS.get(normalized, []))
+        if curated_static:
+            return curated_static
 
     # ── Profile-based generic live fetch (all simple api-key providers) ──
     # Handles any provider registered in providers/ with auth_type="api_key".